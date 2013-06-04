866726e393db70b81a2e7ba01c0c3f0e8ef5c5fe
Pierre de Beaucorps <pierre.de_beaucorps@telecom-sudparis.eu>
2013-06-04 20:49:47 +0200

    classe série

--- src/serie.py
+++ src/serie.py
@@ -1,35 +1,103 @@
 from serial import Serial
 import os
 import time
+from threading import Lock
 
-#liste les chemins trouvés dans /dev
-sources = os.popen('ls -1 /dev/ttyUSB* 2> /dev/null').readlines()
-sources.extend(os.popen('ls -1 /dev/ttyACM* 2> /dev/null').readlines())
-
-for k in range(len(sources)):
-    sources[k] = sources[k].replace("\n","")
+class Serie:
     
-baudrate = 9600
+    baudrate = 9600
+    
+    def __init__(self):
+        
+        #mutex d'accès à la série
+        self.mutex = Lock()
+        
+        #périphérique série de l'imprimante (une fois la carte trouvée)
+        self.serie = None
 
-print("test sur "+sources[0])
-serie = Serial(sources[0], baudrate, timeout=0.1)
+        #recherche du port série
+        self.attribuer()
+        
+    def _clean_string(self, chaine):
+        """
+        supprime des caractères spéciaux sur la chaine
+        """
+        return chaine.replace("\n","").replace("\r","").replace("\0","")         
 
-#vide le buffer série coté pc
-serie.flushInput()
+    def attribuer(self):
+        #liste les chemins trouvés dans /dev
+        sources = os.popen('ls -1 /dev/ttyUSB* 2> /dev/null').readlines()
+        sources.extend(os.popen('ls -1 /dev/ttyACM* 2> /dev/null').readlines())
 
-def _clean_string( chaine):
-    """
-    supprime des caractères spéciaux sur la chaine
-    """
-    return chaine.replace("\n","").replace("\r","").replace("\0","")         
+        for k in range(len(sources)):
+            sources[k] = sources[k].replace("\n","")
+            
+        for source in sources:
+            serie = Serial(source, Serie.baudrate, timeout=0.1)
 
-#récupération de l'identifiant
-serie.write(bytes("@0:ping\r","utf-8"))
-print(_clean_string(str(serie.readline(),"utf-8")))
-print(_clean_string(str(serie.readline(),"utf-8")))
-print(_clean_string(str(serie.readline(),"utf-8")))
+            #vide le buffer série coté pc
+            serie.flushInput()
 
-id = input("id ?")
+            #clean buffer et récupération de l'identifiant
+            serie.write(bytes("@0:ping\r","utf-8"))
+            
+            #il faut vider le buffer de 5 lignes. 
+            #Si on ne recoit pas une trame contenant #RFIDReader au delà, ce n'est pas le bon périphérique
+            tentatives = 0
+            rep = []
+            while (len(rep)<2 or not rep[1] == "#RFIDReader") and tentatives < 6:
+                rep = self._clean_string(str(serie.readline(),"utf-8")).split(":")
+                tentatives += 1
+            
+            if tentatives <= 6:
+                
+                #enregistrement du périphérique
+                self.id = rep[0][1]
+                self.serie = serie
+            
+                #évacuation du prompt
+                serie.readline()
+                
+                #on n'en cherche pas d'autres
+                break
+                
+        if not self.serie:
+            raise Exception("Redbee non trouvée sur la série !")
+            
+        
+    def communiquer(self, destinataire, message, nb_lignes_reponse):
+    
+        self.serie.write(bytes("@0/"+str(destinataire)+":"+message+"\r","utf-8"))
+        
+        reponse = []
+        rep = ''
+        
+        #première trame non vide
+        while rep == '':
+            rep = self._clean_string(str(self.serie.readline(),"utf-8"))
+            time.sleep(0.1)
+            
+        #dernière trame non vide
+        while not rep == '':
+            reponse.append(rep)
+            rep = self._clean_string(str(self.serie.readline(),"utf-8"))
+            
+        #trames ayant du sens
+        t = 0
+        while t < len(reponse):
+            #supprime les trames ne respectant pas le format attendu
+            if not reponse[t][:2] == "@"+self.id:
+                del reponse[t]
+            else:
+                t += 1
+            
+        #supprime les doublons
+        reponse = list(set(reponse))
+        
+        return reponse
+        
+        
+serie = Serie()
 
 while 1:
     ordre = input(">")
@@ -39,11 +107,5 @@ while 1:
         break
         
     if not ordre == "":
-        serie.write(bytes("@0/"+str(id)+":"+ordre+"\r","utf-8"))
+        print(serie.communiquer(serie.id, ordre, 1))
     
-    #ligne vide
-    serie.readline()
-    
-    rep = _clean_string(str(serie.readline(),"utf-8"))
-    
-    print(rep)
